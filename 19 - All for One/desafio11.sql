2d27ee1726519299b84546a914b6edf02e1578fb
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2021-12-07 01:48:59 -0400

    Adicionado o projeto All for One.

---
+++ 19 - All for One/desafio11.sql
@@ -0,0 +1,1 @@
+select notes from northwind.purchase_orders where notes between '30' and 'Purchase generated based on Order #39';
---
+++ 19 - All for One/desafio16.sql
@@ -0,0 +1,1 @@
+select submitted_date from northwind.purchase_orders where date(submitted_date) between '2006-01-26' and '2006-03-31';
